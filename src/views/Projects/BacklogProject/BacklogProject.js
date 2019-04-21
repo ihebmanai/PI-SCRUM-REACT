55724e020317720d07ad0ba799a00a14e108656a
oumaymahb <oumayma.habouri@gmail.com>
2019-04-21 12:22:37 +0100

    Update BacklogProject.js

--- src/views/Projects/BacklogProject/BacklogProject.js
+++ src/views/Projects/BacklogProject/BacklogProject.js
@@ -36,13 +36,30 @@ onSubmit(p) {
   this.setState({
   })
 }
+handleUpdateSpecificRow = (idx) => () => {
+  console.log("iii"+idx)
+  console.log("iiiiiii"+ this.state.userStories)
+  const userStories = [...this.state.userStories]
+
+  this.setState({ userStories })
+  axios.put("http://localhost:3000/backlogProject/updateUserStory/5cbc4de40942e908ec256b88/"+idx,{
+    priority: this.state.userStories.priority,
+    timeestimation:this.state.userStories.timeestimation})
+  .then((response) => {
+    
+    console.log("updated");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+ 
+}
 handleRemoveSpecificRow = (idx) => () => {
   console.log("iii"+idx)
   const userStories = [...this.state.userStories]
   userStories.splice(idx, 1)
   this.setState({ userStories })
-  console.log("aa"+this.state.userStories.priority)
-  axios.delete("http://localhost:3000/userStoryP/delete/"+idx)
+  axios.put("http://localhost:3000/backlogProject/delete/5cbc3978f5fbbe338870a59a/"+idx)
   .then((response) => {
     
     console.log("deleted");
@@ -56,13 +73,14 @@ componentDidMount() {
   var self = this;
   axios.get("http://localhost:3000/project/getBacklog/5cbad555e7622d2ab4868c60")
     .then((response) => {
-      console.log(response.data[0]);
+      console.log(response.data[0].userstories[0]);
+      
       self.setState({
           userStories:response.data[0].userstories,
           ID:response.data[0]._id,
       })
       console.log(this.state.ID);
-      console.log(this.state.userStories);
+      console.log("aaa"+this.state.userStories);
     })
     .catch(error => {
       console.log(error);
@@ -108,7 +126,7 @@ componentDidMount() {
                                                    onChange={this.onChange}/> </td>
               <td><Input type="text" id="_id" name="_id" value={this.state.userStories.timeestimation} placeholder={userstory.timeestimation}
                                                    onChange={this.onChange}/></td>
-               <td> <Button block color="primary">Edit</Button></td>
+               <td> <Button block color="primary" onClick={this.handleUpdateSpecificRow(userstory._id)}>Edit</Button></td>
               <td><Button block color="danger" onClick={this.handleRemoveSpecificRow(userstory._id)}>Delete</Button></td>
           
                     
