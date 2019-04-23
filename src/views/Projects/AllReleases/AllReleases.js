7ecb520f2de75360f41a457961d66eb6d48f3fb3
oumaymahb <oumayma.habouri@gmail.com>
2019-04-23 13:54:13 +0100

    23/4

--- src/views/Projects/AllReleases/AllReleases.js
+++ src/views/Projects/AllReleases/AllReleases.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Badge, Card, CardBody, CardFooter, CardHeader, Col, Row, Collapse, Fade } from 'reactstrap';
 import { AppSwitch } from '@coreui/react'
-
+import axios from "axios";
 class AllReleases extends Component {
   constructor(props) {
     super(props);
@@ -11,10 +11,43 @@ class AllReleases extends Component {
     this.state = {
       collapse: true,
       fadeIn: true,
-      timeout: 300
+      timeout: 300,
+      
+      releases:[{
+        goals:'',
+    status:'',
+    startingDate:'',
+    releaseDate:'',
+    numberSprint:'',
+    userstories: []
+      }
+      ]
     };
   }
-
+  onChange = idx => e => {
+    const { name, value } = e.target;
+    const releases = [...this.state.releases];
+    releases[idx] = {
+      [name]: value
+    };
+    this.setState({
+      releases
+    });
+  }
+  componentDidMount(e) {
+    var self = this;
+    axios.get("http://localhost:3000/release")
+      .then((response) => {
+        console.log(response.data);
+       
+        self.setState({
+          releases:response.data})
+        console.log(this.state.releases);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
   toggle() {
     this.setState({ collapse: !this.state.collapse });
   }
@@ -24,58 +57,34 @@ class AllReleases extends Component {
   }
 
   render() {
+
     return (
       <div className="animated fadeIn">
   
     
         <Row>
-          <Col xs="12" sm="6" md="4">
+        {
+                      this.state.releases.map((release,idx) => {
+                        var d2 = new Date(release.releaseDate);
+                        var formattedDate2 =  d2.getFullYear()+"-" + (d2.getMonth() + 1) + "-" + d2.getDate() 
+
+                     return (
+          <Col xs="12" sm="6" md="4"  key={idx}>
             <Card className="text-white bg-primary">
               <CardHeader>
-                Release 1 
+                Release {idx+1}
               </CardHeader>
               <CardBody style={{backgroundColor:"white",color:"black"}}>
-               <p>Project: Java project</p> 
-               <p>Goals: Develop an e-learning platfomr with java</p>
-               <p>Status:Not started</p>
-                <p>Starting date: 2019-05-01</p>
-                <p>Release date:2019-09-30</p>
-                <p>Number of sprints:5</p>
+               <p >Goals : {this.state.releases[idx].goals}</p> 
+               <p>Status: {this.state.releases[idx].status}</p> 
+               <p>Number of Sprints: {this.state.releases[idx].numberSprint}</p> 
+               <p>Release Date: {formattedDate2}</p> 
+               
                 
               </CardBody>
             </Card>
           </Col>
-          <Col xs="12" sm="6" md="4">
-            <Card className="text-white bg-success">
-              <CardHeader>
-                Release 2
-              </CardHeader>
-              <CardBody style={{backgroundColor:"white",color:"black"}}>
-              <p>Project: Java project</p> 
-               <p>Goals: Develop an e-learning platfomr with java</p>
-               <p>Status:Not started</p>
-                <p>Starting date: 2019-05-01</p>
-                <p>Release date:2019-09-30</p>
-                <p>Number of sprints:5</p>
-              </CardBody>
-            </Card>
-          </Col>
-        
-          <Col xs="12" sm="6" md="4">
-            <Card className="text-white bg-danger">
-              <CardHeader>
-                Release 3
-              </CardHeader>
-              <CardBody style={{backgroundColor:"white",color:"black"}}>
-              <p>Project: Java project</p> 
-               <p>Goals: Develop an e-learning platfomr with java</p>
-               <p>Status:Not started</p>
-                <p>Starting date: 2019-05-01</p>
-                <p>Release date:2019-09-30</p>
-                <p>Number of sprints:5</p>
-              </CardBody>
-            </Card>
-          </Col>
+                     ) }) }
           <Col xs="12" sm="6" md="4">
             <Fade timeout={this.state.timeout} in={this.state.fadeIn}>
               <Card>
--- src/views/Projects/ProjectsList/ProjectList.js
+++ src/views/Projects/ProjectsList/ProjectList.js
@@ -5,7 +5,10 @@ import axios from "axios";
 class ProjectList extends Component {
   constructor(props) {
     super(props);
+    this.onChange = this.onChange.bind(this);
     this.state = {
+      scrumMasterName:'',
+      prodcutOwnerName:'',
       projects:[{
       projectName: '',
       startingDate:'',
@@ -18,18 +21,51 @@ class ProjectList extends Component {
       }]
     }
   }
+  onChange = idx => e => {
+    const { name, value } = e.target;
+    const projects = [...this.state.projects];
+    projects[idx] = {
+      [name]: value
+    };
+    this.setState({
+      projects
+    });
+  }
   componentDidMount(e) {
     var self = this;
     axios.get("http://localhost:3000/project")
       .then((response) => {
-        console.log(response.data[0]);
+        console.log(response.data);
+       
         self.setState({
-          projects:response.data[0]})
+          projects:response.data})
         console.log(this.state.projects);
       })
       .catch(error => {
         console.log(error);
       });
+      axios.get("http://localhost:3000/user/5cbacf5e75c4bc1df0a5bf6e")
+      .then((response) => {
+        console.log(response.data);
+        var scrumName=response.data.firstName+" "+response.data.lastName
+        self.setState({
+          scrumMasterName:scrumName})
+        console.log(this.state.scrumMasterName);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      axios.get("http://localhost:3000/user/5cbacf7475c4bc1df0a5bf6f")
+      .then((response) => {
+        console.log(response.data);
+        var productOwnerrName=response.data.firstName+" "+response.data.lastName
+        self.setState({
+          productOwnerName:productOwnerrName})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      
      
   }
   render() {
@@ -58,17 +94,28 @@ class ProjectList extends Component {
                   <tbody>
                   {
                       this.state.projects.map((project,idx) => {
-            return (
+                        var d = new Date(project.startingDate);
+                        var formattedDate =  d.getFullYear()+"-" + (d.getMonth() + 1) + "-" + d.getDate() 
+                        var d2 = new Date(project.endDate);
+                        var formattedDate2 =  d2.getFullYear()+"-" + (d2.getMonth() + 1) + "-" + d2.getDate() 
+                        console.log("ddddddddddddd"+formattedDate)
+                        var c="success"
+                        if ((project.status) == "not started")
+                        c="danger"
+
+                     return (
                   <tr key={idx}>
-                    <td>{project.projectName}</td>
-                    <td>{project.key}</td>
-                    <td>{project.description}</td>
-                    <td>{project.startingDate}</td>
-                    <td>{project.endDate}</td>
-                    <td>{project.prductOwner}</td>
-                    <td>{project.scrumMaster}</td>
+                    <td>{this.state.projects[idx].projectName}</td>
+                    <td>{this.state.projects[idx].key}</td>
+                    <td>{this.state.projects[idx].description}</td>
+                    <td>{formattedDate}</td>
+                    <td>{formattedDate2}</td>
+                    <td>{this.state.productOwnerName}</td>
+                    <td>{this.state.scrumMasterName}</td>
                     <td>
-                      <Badge color="success">{project.status}</Badge>
+                     
+                   
+                      <Badge color={c}>{project.status}</Badge>
                     </td>
                   </tr>
                        )})
